df2ce4572ab15a1f7e82445748ef9a1c8048c924
VinitPania <vinit.pania@cloverinfotech.com>
2023-09-13 19:36:56 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import { Text } from "react-native"
+const App = () => {
+ return <Text style={{fontSize:40}}>Hello Khalid Ansari from Android World</Text>
+}
+
+export default App
